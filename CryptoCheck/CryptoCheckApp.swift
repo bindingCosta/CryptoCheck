6ccd1f0db7c7be612a7994cdbcc30774a9cf2000
marcelo.costa <costanetomj@gmail.com>
2024-12-10 01:14:51 -0300

    Initial Commit

---
+++ CryptoCheck/CryptoCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCheckApp.swift
+//  CryptoCheck
+//
+//  Created by Marcelo Costa on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
